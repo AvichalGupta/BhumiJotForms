2da688cceebbd4f27f88ce6bf2a29edec1a278b0
Avichal Gupta <avichalgupta1999@gmail.com>
2021-03-06 12:48:50 +0300

    html page added

--- index.js
+++ index.js
@@ -4,10 +4,13 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded());
 
+app.get("/",(req,res)=>{
+    res.sendFile(`${__dirname}/index.html`)
+})
 app.listen(process.env.PORT||3000,()=>{
     console.log(`Running on port ${process.env.PORT || 3000}`);
 })
 
 app.post("/sendData",(req,res)=>{
-    console.log(req.params);
+    console.log(req);
 })
